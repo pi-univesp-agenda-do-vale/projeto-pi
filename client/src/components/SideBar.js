be669cdfb1d209c771e5d245634b8040cbe52b87
RaphaellReis <raphael_rprvga@hotmail.com>
2022-11-22 20:09:34 -0300

    Ajustes CSS

--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -3,14 +3,14 @@ import './styles.css'
 
 function SideBar(){
     return (
-        <ul>
+        <ul className="nodot">
             <li>
                 <p>Usuário</p>
                 <ul className="ulsidebar">
-                    <li className="lisidebar">
+                    <li>
                     <a href="/dashboard/users">Cadastrar</a>
                     </li>
-                    <li className="lisidebar">
+                    <li>
                     <a href="/dashboard/usersview">Listar</a>
                     </li>
                 </ul>
@@ -18,10 +18,10 @@ function SideBar(){
             <li>
                 <p>Categoria</p>
                 <ul className="ulsidebar">
-                    <li className="lisidebar">
+                    <li>
                     <a href="/dashboard/categories">Cadastrar</a>
                     </li>
-                    <li className="lisidebar">
+                    <li>
                     <a href="/dashboard/categoriesview">Listar</a>
                     </li>
                 </ul>
@@ -29,10 +29,10 @@ function SideBar(){
             <li>
                 <p>Eventos</p>
                 <ul className="ulsidebar">
-                    <li className="lisidebar">
+                    <li>
                     <a href="/dashboard/events">Cadastrar</a><br/>
                     </li>
-                    <li className="lisidebar">
+                    <li>
                     <a href="/dashboard/eventsview">Listar</a>
                     </li>
                 </ul>
